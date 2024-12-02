a310a6f6969102278f9cadadda2f66b57a2e5ee9
Heiko Sippel <heiko.sippel.ext@msg.group>
2024-12-02 11:01:25 +0100

    feat: IFS-4360: removed IsyLogger

--- src/main/java/de/bund/bva/isyfact/polling/PollingMBean.java
+++ src/main/java/de/bund/bva/isyfact/polling/PollingMBean.java
@@ -16,39 +16,42 @@
  */
 package de.bund.bva.isyfact.polling;
 
-import java.util.Date;
-
-import org.springframework.jmx.export.annotation.ManagedAttribute;
-import org.springframework.jmx.export.annotation.ManagedResource;
-
 import de.bund.bva.isyfact.datetime.util.DateTimeUtil;
-import de.bund.bva.isyfact.logging.IsyLogger;
-import de.bund.bva.isyfact.logging.IsyLoggerFactory;
 import de.bund.bva.isyfact.polling.common.exception.PollingClusterKonfigurationException;
 import de.bund.bva.isyfact.polling.common.exception.PollingClusterUnbekanntException;
 import de.bund.bva.isyfact.polling.common.konstanten.EreignisSchluessel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jmx.export.annotation.ManagedAttribute;
+import org.springframework.jmx.export.annotation.ManagedResource;
+
+import java.util.Date;
 
 /**
  * MBean die Bereitstellung der vom Polling-Verwalter benötigten Informationen.
- *
  */
 @ManagedResource(description = "Diese MBean dient zum Abruf der zur Synchronisation "
-    + "von Polling-Aktivitäten benötigten Informationen.")
+        + "von Polling-Aktivitäten benötigten Informationen.")
 public class PollingMBean {
-    /** Der Logger dieser Klasse. */
-    private static final IsyLogger LOG = IsyLoggerFactory.getLogger(PollingMBean.class);
+    /**
+     * Der Logger dieser Klasse.
+     */
+    private static final Logger LOG = LoggerFactory.getLogger(PollingMBean.class);
 
-    /** Der Polling-Verwalter. Wird über Spring gesetzt. */
+    /**
+     * Der Polling-Verwalter. Wird über Spring gesetzt.
+     */
     private PollingVerwalter pollingVerwalter;
 
-    /** ID des Polling-Clusters, für den die Werte abgerufen werden können. Wird über Spring gesetzt. */
+    /**
+     * ID des Polling-Clusters, für den die Werte abgerufen werden können. Wird über Spring gesetzt.
+     */
     private volatile String clusterId;
 
     /**
      * Setzt das Attribut 'pollingVerwalter'.
      *
-     * @param pollingVerwalter
-     *            der Attributwert.
+     * @param pollingVerwalter der Attributwert.
      */
     public void setPollingVerwalter(PollingVerwalter pollingVerwalter) {
         this.pollingVerwalter = pollingVerwalter;
@@ -56,8 +59,8 @@ public class PollingMBean {
 
     /**
      * Setzt das Feld 'clusterId'.
-     * @param clusterId
-     *            Neuer Wert für clusterId
+     *
+     * @param clusterId Neuer Wert für clusterId
      */
     public void setClusterId(String clusterId) {
         this.clusterId = clusterId;
@@ -69,7 +72,7 @@ public class PollingMBean {
      * @return Zeitpunkt der letzten Polling-Aktivität.
      */
     @ManagedAttribute(
-        description = "Liefert den Zeitpunkt der letzten Polling-Aktivität für einen Polling-Cluster.")
+            description = "Liefert den Zeitpunkt der letzten Polling-Aktivität für einen Polling-Cluster.")
     public Date getZeitpunktLetztePollingAktivitaet() {
         try {
             return new Date(this.pollingVerwalter.getZeitpunktLetztePollingAktivitaet(this.clusterId));
@@ -89,11 +92,11 @@ public class PollingMBean {
      * @return Zeitpunkt der letzten Polling-Aktivität.
      */
     @ManagedAttribute(
-        description = "Liefert die vergangene Zeit seit der letzten Polling-Aktivität für einen Polling-Cluster in Millisekunden.")
+            description = "Liefert die vergangene Zeit seit der letzten Polling-Aktivität für einen Polling-Cluster in Millisekunden.")
     public long getZeitraumLetztePollingAktivitaet() {
         try {
             long zeitpunktLetztePollingAktivitaet =
-                this.pollingVerwalter.getZeitpunktLetztePollingAktivitaet(this.clusterId);
+                    this.pollingVerwalter.getZeitpunktLetztePollingAktivitaet(this.clusterId);
             if (zeitpunktLetztePollingAktivitaet == 0) {
                 // 0 bedeutet, dass dieser Knoten noch nie gepollt hat
                 return Long.MAX_VALUE;
@@ -102,7 +105,7 @@ public class PollingMBean {
             }
         } catch (PollingClusterKonfigurationException e) {
             LOG.error(EreignisSchluessel.POLLING_CLUSTER_KONFIG_FEHLER,
-                "PollingClusterKonfigurationException", e);
+                    "PollingClusterKonfigurationException", e);
             return Long.MAX_VALUE;
         } catch (PollingClusterUnbekanntException e) {
             LOG.error(EreignisSchluessel.POLLING_CLUSTER_UNBEKANNT, "PollingClusterUnbekanntException", e);
